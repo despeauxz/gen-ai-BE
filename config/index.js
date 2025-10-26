21858c6a27827b0b93c0911003c566c3d2c6260e
vbv000406 <despeauxz@gmail.com>
2025-10-26 22:49:12 +0100

    Modify env variables

--- config/index.js
+++ config/index.js
@@ -25,7 +25,7 @@ const config =  (nodeEnv, envTag) => ({
 	dbPassword: getValue("POSTGRES_PASSWORD"),
 	dbUsername: getValue("POSTGRES_USER"),
 	dbName: getValue("POSTGRES_DATABASE"),
-	dbPort: 5432,
+	dbPort: getValue("POSTGRES_PORT"),
 	dbHost: getValue("POSTGRES_HOST"),
 	dbMaxConnections:  getValue("POSTGRES_MAX_CONNECTIONS"),
 	// app configuration
